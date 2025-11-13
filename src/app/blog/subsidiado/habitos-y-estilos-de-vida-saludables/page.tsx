1b606b75049f87941e291f6f5e29173bc5adcb4c
VICTOR ALFREDO BOTINA JOJOA <victorbotina@emssanareps.co>
2025-11-13 21:59:34 +0000

    blog

--- src/app/blog/subsidiado/habitos-y-estilos-de-vida-saludables/page.tsx
+++ src/app/blog/subsidiado/habitos-y-estilos-de-vida-saludables/page.tsx
@@ -1,4 +1,3 @@
-// THIS FILE IS NO LONGER IN USE AND CAN BE SAFELY DELETED
 // The dynamic route at src/app/blog/subsidiado/[slug]/page.tsx now handles all articles.
 export default function HabitosSaludablesPage() {
     return null;
