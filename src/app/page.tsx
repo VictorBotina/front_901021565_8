941b03b1e85daf6d731bf24a1e7fc7febca51c32
VICTOR ALFREDO BOTINA JOJOA <victorbotina@emssanareps.co>
2025-11-24 21:42:15 +0000

    ubica "/home/user/studio/src/app/blog/subsidiado/libre-escogencia-de-ips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Hero, type Slide } from "@/components/layout/home/Hero";
 import { InfoCards } from "@/app/(home)/(sections)/InfoCards";
 import { FeedbackSection } from "@/app/(home)/(sections)/FeedbackSection";
 import { PlaceHolderImages } from "@/lib/placeholder-images";
+import { StrapiApiTester } from "./(home)/(sections)/StrapiApiTester";
 
 export default function Home() {
   // Define el contenido para cada slide del carrusel
@@ -34,6 +35,7 @@ export default function Home() {
       <Hero slides={heroSlides} />
       <InfoCards />
       <FeedbackSection />
+      <StrapiApiTester />
     </>
   );
 }
