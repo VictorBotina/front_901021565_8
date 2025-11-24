b06ebdff13d172566410a778433d63f3486ded8f
VICTOR ALFREDO BOTINA JOJOA <victorbotina@emssanareps.co>
2025-11-24 20:34:21 +0000

    Verifica que todos los componentes internos mantengan su diseño y compor

--- src/app/afiliados/page.tsx
+++ src/app/afiliados/page.tsx
@@ -292,8 +292,8 @@ export default function AfiliadosPage() {
             </div>
           </section>
         </div>
-        <ArticleSection title="Últimas noticias" />
       </main>
+      <ArticleSection title="Últimas noticias" />
     </div>
   );
 }
