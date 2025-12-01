13cb885a19f70fbabf0e3c6523de598b5f1e7154
VICTOR ALFREDO BOTINA JOJOA <victorbotina@emssanareps.co>
2025-12-01 19:12:10 +0000

    Cambia el texto "Cursos de vida" a "Cursos de Vida: Etapas y Cuidados Es

--- src/app/afiliados/subsidiado/salud/page.tsx
+++ src/app/afiliados/subsidiado/salud/page.tsx
@@ -16,7 +16,6 @@ import {
   PersonStanding,
   User,
   School,
-  GitCommit,
   GraduationCap,
   Briefcase,
   Heart,
@@ -94,7 +93,7 @@ export default function CuidadoSaludLandingPage() {
           {/* Columna Cursos de Vida */}
           <section>
             <h2 className="text-3xl font-bold text-center mb-8">
-              Cursos de Vida
+              Cursos de Vida: Etapas y Cuidados Esenciales
             </h2>
             <Card>
               <CardHeader>
